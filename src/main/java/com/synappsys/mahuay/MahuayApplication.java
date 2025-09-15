6d9930cda776b25f9427e4a27a5c4dfa640237a7
Eric Y <zepeycire@gmail.com>
2025-09-14 21:43:32 -0500

    initial commit

---
+++ src/main/java/com/synappsys/mahuay/MahuayApplication.java
@@ -0,0 +1,13 @@
+package com.synappsys.mahuay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahuayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahuayApplication.class, args);
+	}
+
+}
